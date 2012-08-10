5dcd694b5ef20d1b1d06aefb990dd506767d98ae
Alex Kapranoff <alex@kapranoff.ru>
2012-08-11 03:22:37 +0400

    Dump Data::Dump and allow testing non-ascii names

--- test.pl
+++ test.pl
@@ -1,17 +1,23 @@
 #! /usr/bin/perl
 use uni::perl;
-use Data::Dump;
+use Data::Dumper;
 use HTTP::DAV;
 use Config::Tiny;
 
 my $cfg = Config::Tiny->read('disk.conf')
     or die $!;
 
+binmode(STDOUT, ":utf8");
+my $filename = $ARGV[0]
+    or die "Usage: $0 <filename>\n";
+
+utf8::decode $filename;
+
 $ENV{SHUTTER_ROOT} = '/usr';
 require 'YandexDisk.pm';
 
 my $yd = {};
 
-YandexDisk::upload($yd, $ARGV[0], $cfg->{webdav}->{username}, $cfg->{webdav}->{passwd});
+YandexDisk::upload($yd, $filename, $cfg->{webdav}->{username}, $cfg->{webdav}->{passwd});
 
-dd $yd;
+print Dumper($yd);
